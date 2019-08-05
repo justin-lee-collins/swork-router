ad6a7a3d4d44d3a40fd03ff66aff24fbf7e18641
Justin Collins <connect.with.justin@gmail.com>
2019-08-04 22:41:07 -0500

    Added comments

--- src/index.ts
+++ src/index.ts
@@ -2,76 +2,166 @@ import * as pathToRegExp from "path-to-regexp";
 import { FetchContext, Middleware } from "swork";
 import { configuration } from "swork/dist/configuration";
 
+/**
+ * Defines the configuration used to by a Router instance.
+ *
+ * @export
+ * @interface IRouterConfiguration
+ */
 export interface IRouterConfiguration {
     prefix: string;
 }
 
 interface IRouterContext {
+    /**
+     * The params object populated by the matching routes.
+     */
     params: { [key: string]: string };
-} 
+}
 
-// declare module "swork" {
-//     // tslint:disable-next-line:interface-name
-//     export interface FetchContext {
-//         params: { [key: string]: string };
-//     }
-// }
-
+/**
+ * The router context; an extension of FetchContext.
+ */
 export type RouterContext = FetchContext & IRouterContext;
+
+/**
+ * The router middleware definition; an extension of Middleware.
+ */
 export type RouterMiddleware = (context: RouterContext, next: () => Promise<void>) => void | Promise<void>;
 
 type HttpMethod = "HEAD" | "OPTIONS" | "GET" | "PUT" | "PATCH" | "POST" | "DELETE";
 
+/**
+ * Router intended to be used with swork. Allows definition of paths and middleware to handle matching requests.
+ *
+ * @export
+ * @class Router
+ */
 export class Router {
     protected config: IRouterConfiguration;
     protected middlewares: Middleware[] = [];
 
+    /**
+     * Creates an instance of Router.
+     * @param {IRouterConfiguration} [config]
+     * @memberof Router
+     */
     constructor(config?: IRouterConfiguration) {
         this.config = Object.assign({
             prefix: "",
         }, config!);
     }
 
+    /**
+     * Defines a head request handler.
+     *
+     * @param {string} path
+     * @param {RouterMiddleware} middleware
+     * @returns {Router}
+     * @memberof Router
+     */
     public head(path: string, middleware: RouterMiddleware): Router {
         this.addMiddleware(path, ["HEAD"], middleware);
         return this;
     }
 
+    /**
+     * Defines an options request handler.
+     *
+     * @param {string} path
+     * @param {RouterMiddleware} middleware
+     * @returns {Router}
+     * @memberof Router
+     */
     public options(path: string, middleware: RouterMiddleware): Router {
         this.addMiddleware(path, ["OPTIONS"], middleware);
         return this;
     }
 
+    /**
+     * Defines a get request handler.
+     *
+     * @param {string} path
+     * @param {RouterMiddleware} middleware
+     * @returns {Router}
+     * @memberof Router
+     */
     public get(path: string, middleware: RouterMiddleware): Router {
         this.addMiddleware(path, ["GET"], middleware);
         return this;
     }
 
+    /**
+     * Defines a post request handler.
+     *
+     * @param {string} path
+     * @param {RouterMiddleware} middleware
+     * @returns {Router}
+     * @memberof Router
+     */
     public post(path: string, middleware: RouterMiddleware): Router {
         this.addMiddleware(path, ["POST"], middleware);
         return this;
     }
 
+    /**
+     * Defines a patch request handler.
+     *
+     * @param {string} path
+     * @param {RouterMiddleware} middleware
+     * @returns {Router}
+     * @memberof Router
+     */
     public patch(path: string, middleware: RouterMiddleware): Router {
         this.addMiddleware(path, ["PATCH"], middleware);
         return this;
     }
 
+    /**
+     * Defines a put request handler.
+     *
+     * @param {string} path
+     * @param {RouterMiddleware} middleware
+     * @returns {Router}
+     * @memberof Router
+     */
     public put(path: string, middleware: RouterMiddleware): Router {
         this.addMiddleware(path, ["PUT"], middleware);
         return this;
     }
 
+    /**
+     * Defines a delete request handler.
+     *
+     * @param {string} path
+     * @param {RouterMiddleware} middleware
+     * @returns {Router}
+     * @memberof Router
+     */
     public delete(path: string, middleware: RouterMiddleware): Router {
         this.addMiddleware(path, ["DELETE"], middleware);
         return this;
     }
 
+    /**
+     * Defines a request handler for all HTTP verbs.
+     *
+     * @param {string} path
+     * @param {RouterMiddleware} middleware
+     * @returns {Router}
+     * @memberof Router
+     */
     public all(path: string, middleware: RouterMiddleware): Router {
         this.addMiddleware(path, ["HEAD", "OPTIONS", "GET", "PUT", "PATCH", "POST", "DELETE"], middleware);
         return this;
     }
 
+    /**
+     * Builds the middlewares from the router to be used by swork.
+     *
+     * @returns {Middleware[]}
+     * @memberof Router
+     */
     public routes(): Middleware[] {
         return this.middlewares;
     }
